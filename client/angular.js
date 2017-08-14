532b88ef503a4dfd5205d7a839b450b81939705e
Innovationbham_2 <Innovationbham_2@UABiLab-2s-iMac.local>
2017-08-14 10:28:18 -0500

    added nav bar

--- client/angular.js
+++ client/angular.js
@@ -11,13 +11,14 @@ app.config(function($routeProvider){
         templateUrl: "../views/role.html"
     })
 });
-app.controller("ChampionGetController",['$scope', '$http','$location',function($scope,$http,$location){
+app.controller("ChampionGetController",['$scope', '$http','$location','$sce',function($scope,$http,$location,$sce){
     console.log('i have been got')
         console.log('git clicked')
         $http.get('http://localhost:3000/api/')
             .then(function(response){
                 console.log(response.data)
                 $scope.champions=response.data
+              //  $scope.Spotlight = $sce.trustAsResourceUrl($scope.Spotlight);
     });
             $scope.getChampion=function(id){
             console.log(id)
@@ -28,7 +29,7 @@ app.controller("ChampionGetController",['$scope', '$http','$location',function($
             $location.path("/champions/role/" + Role)
         };
 }])
-app.controller("SingleChampion",['$scope', '$routeParams', '$http', function($scope, $routeParams, $http){
+app.controller("SingleChampion",['$scope', '$routeParams', '$http','$sce', function($scope, $routeParams, $http,$sce){
      var id=$routeParams.id;
     $http.get('http://localhost:3000/api/'+id)
         .then(function(response){
